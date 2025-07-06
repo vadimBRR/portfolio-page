21477ee0196019aab0973fc1e6a4c7199bf2c90b
vadimBRR <vadmabos20@gmail.com>
2025-07-06 12:26:25 +0200

    delete light theme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	output: 'export',
+	basePath: '/portfolio-page',
+	assetPrefix: '/portfolio-page/',
+}
 
-export default nextConfig;
+export default nextConfig
